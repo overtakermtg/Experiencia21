28e190620046277f16a136ffb4b6440ab82a3775
Overtaker <angel.overtaker@gmail.com>
2017-12-07 10:03:30 -0300

    ultimos cambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
 #convierte la pagina uno en index
-  root 'pages#one'
+  
 
   get 'pages/two'
-
   get 'pages/one'
-
   get 'pages/three'
+
+
+root 'pages#one'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
